9f5f023b38a605662bc82b742e0e6c1cba2bf39c
qkqakseb <parknanju6376@gmail.com>
2022-12-21 17:31:13 +0900

    array 올림

---
+++ WhatIsArray/Class1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsArray
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+
+
+            // 야구게임 (마무리 하기)
+            // 프로그램이 세가지 숫자를 랜덤해서 가지고 있어야된다.(세자리 각각 다른 숫자가 들어가야 됨)
+            // 사용되는 숫자는 0에서 9까지 서로 다른 숫자
+            // 0은 사용하지 않는다.
+            // 숫자는 맞지만 위치가 틀렸을때는 볼
+            // 숫자와 위치가 전부 맞으면 스트라이크
+            // 숫자와 위치가 전부 틀리면 "OUT"
+            // 틀린 숫자는 제끼고 남은 숫자끼리 조합
+
+
+            // 프로그램이 세가지 숫자를 랜덤해서 가지고 있어야된다.(세자리 각각 다른 숫자가 들어가야 됨)
+
+            // 사용되는 숫자는 0에서 9까지 서로 다른 숫자
+
+            //Random allprogramNumber = new Random();
+
+            //int programNumber1 = allprogramNumber.Next(0, 10);
+            //int programNumber2 = allprogramNumber.Next(0, 10);
+            //int programNumber3 = allprogramNumber.Next(0, 10);
+
+            //while (programNumber1 == programNumber2 || programNumber2 == programNumber3 || programNumber3 == programNumber1)
+            //{
+            //    programNumber1 = allprogramNumber.Next(0, 10);
+            //    programNumber2 = allprogramNumber.Next(0, 10);
+            //    programNumber3 = allprogramNumber.Next(0, 10); //다시
+            //}
+
+
+            //int userNumber1 = 0;
+            //int userNumber2 = 0;
+            //int userNumber3 = 0;
+
+            //int loopCount =0;
+
+            //while (loopCount != 9) //loop 횟수를 기록할 변수 선언
+            //{
+            //    Console.WriteLine("첫번째 숫자를 입력해주세요");
+            //    int.TryParse(Console.ReadLine(), out userNumber1);
+
+            //    Console.WriteLine("두번째 숫자를 입력해주세요");
+            //    int.TryParse(Console.ReadLine(), out userNumber2);
+
+            //    while (userNumber1 == userNumber2)
+            //    {
+            //        Console.WriteLine("두번째 숫자를 입력해주세요");
+            //        int.TryParse(Console.ReadLine(), out userNumber1);
+            //    }
+
+            //    Console.WriteLine("세번째 숫자를 입력해주세요");
+            //    int.TryParse(Console.ReadLine(), out userNumber3);
+
+            //    while (userNumber1 == userNumber3 || userNumber2 == userNumber3)
+            //    {
+            //        Console.WriteLine("세번째 숫자를 입력해주세요");
+            //        int.TryParse(Console.ReadLine(), out userNumber1);
+            //    }
+
+            //    int strike = 0;
+            //    int ball = 0;
+            //    int Out = 0;
+
+            //    if (programNumber1 == userNumber1)
+            //    {
+            //        strike++;
+            //    }
+            //    else if (userNumber1 == programNumber2 || userNumber1 == programNumber3)
+            //    {
+            //        ball++;
+            //    }
+            //    else
+            //    {
+            //        Out++;
+            //    }
+
+            //    if (programNumber2 == userNumber2)
+            //    {
+            //        strike++;
+            //    }
+            //    else if (userNumber2 == programNumber1 || userNumber2 == programNumber3)
+            //    {
+            //        ball++;
+            //    }
+            //    else
+            //    {
+            //        Out++;
+            //    }
+
+            //    if (programNumber3 == userNumber3)
+            //    {
+            //        strike++;
+            //    }
+            //    else if (userNumber3 == programNumber1 || userNumber3 == programNumber2)
+            //    {
+            //        ball++;
+            //    }
+            //    else
+            //    {
+            //        Out++;
+            //    }
+            //}//while
+            //Console.WriteLine("종료합니다.");
+
+            /*
+             * 컬렉션
+             * 이름 하나로 데이터 여러 개를 담을 수 있는 자료 구조를 컬렉션(Collection) 또는
+             * 컨테이너(Container)라고 한다. c#에서 다루는 컬렉션은 배열(Array), 리스트(List),
+             * 사전(Dictinary) 등이 있다.
+             * 
+             * 배열
+             * 배열(Array)은 같은 종류의 데이터들이 순차적으로 메모리에 저장되는 자효 구조이다. 각각의 데이터들은
+             * 인덱스(번호)를 사용하여 독립적으로 접근된다. 배열을 사용하면 편리하게 데이터를 모아서 관리할 수 있다.
+             * 
+             * 배열의 특징들
+             * 1. 배열 하나에는 데이터 형식 한 종류만 보관할 수 있다.
+             * 2. 배열은 메모리의연속된 공간을 미리 할당하고, 이를 대괄호([])와 0부터 시작하는 정수형 인덱스를
+             *    사용하여 접근할 수 있다.,
+             * 3. 배열을 선언할 때는 new 키워드로 배열을 생성한 후 사용할 수 있다.
+             * 4. 배열에서 값 하나는 요소(Element) 또는 항목(Item)으로 표현한다.
+             * 5. 필요한 데이터 개수를 정확히 정한다면 메모리를 적게 사용하여 프로그램 크기가 작아지고 성능이
+             *    향상된다.
+             *    
+             *    배열에는 세 가지 종류가 있다.
+             *    1차원 배열 : 배열의 첨자를 하나만 사용하는 배열
+             *    2차원 배열 : 첨자 2개 이상을 사용하는 배열(2차원, 3차원, .....n차원 배열)
+             *    가변(Jagged) 배열: '배열의 배열'이라고도 하며, 이름 하나로 다양한 차원의 배열을 표현할 때 사용한다.
+             */
+
+            //배열의 선언과 초기화
+            int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+
+            //int number1 = 1;
+            //int number2 = 2;
+            //int number3 = 3;
+            //int number4 = 4;
+            //int number5 = 5;
+
+            Console.WriteLine(numbers); 
+
+            //for (int index = 0; index < numbers.Length; index++)
+            //{
+            //    Console.WriteLine($"{ numbers[index]}");
+            //}
+            //foreach (int element in numbers)
+            //{
+            //    Console.WriteLine($"{element}");
+            //}
+        }//Main
+    } // Class
+}
---
+++ WhatIsArray/Program.cs
@@ -0,0 +1,297 @@
+﻿// System 이라는 어셈블리에서 이것 저것 여러 기능을 가져와서 사용 할 거다.
+using System;
+
+//네 프로그램 이름이다. 내가 결정함
+namespace WhatIsArray
+{   
+    // 클래스 라는 것인데, c#에서는 모든 요소들이 클래스 안에 있어야 함
+    internal class Program
+    {
+        // 무조건 1개는 있어야 함 -> c# 콘솔(검은 창)을 사용할 때
+        static void Main(string[] args)
+        {
+            //// 프로그램은 여기서부터 읽기 시작한다. 
+            //Console.WriteLine("Hello, World!");
+
+            //// 3의 배수를 제외한 수
+            //int sumofNumber = 0;
+            ////int variable_ = 100;
+
+            //for (int variable_ = 1; variable_ <= 100; variable_++)
+            //{
+            //    bool isRealMultipleofThree = (variable_ % 3 == 0);
+            //    Console.WriteLine($" {variable_} -> isRealMultipleofThree : {isRealMultipleofThree}");
+
+
+            //    //1~100 숫자 중에서 3의 배수를 제외한 수의 합 구하기
+            //    if (isRealMultipleofThree == false)
+            //    {
+            //        // 3의 배수가 아닌건 다 여기로 온다
+            //        // 여기서 값을 계속 더해주면 된다.
+            //        sumofNumber += variable_;
+            //        Console.WriteLine($"잘 더해지고 있나? : {sumofNumber}");
+            //    }
+            //    else
+            //    {
+            //        // 여기로 온다.
+            //        // 그러면 여기서 코딩
+            //    }
+
+            //    Console.WriteLine();
+            //}   // loop: 100번 도는 루프
+            //// 루프가 끝나면 여기로 오니까 다 끝날 때까지는 더해서 마지막 값을 눈으로
+            //// 보고싶다
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("=================================");
+            //Console.WriteLine($"sumofNumber : {sumofNumber}");
+            //Console.WriteLine("=================================");
+
+
+
+            ////if (/* 여기서 조건문 검사함*/)
+            ////{
+            ////    // 저 조건문이 참(True)이면 여기로 옴
+            ////    // 조건문은 여기서부터 읽기 시작한다.
+            ////    // 조건문은 여기서 끝난다.
+            ////}
+            ////else 
+            ////{
+            ////    // 저 조건문이 거짓(False)이면 여기로 옴
+            ////    // 조건문은 여기서부터 읽기 시작한다.
+            ////    // 조건문은 여기서 끝난다.
+            ////}
+            ////// 조건문을 실행했으면 이제 다시 여기서부터 프로그램 시작한다.
+            ////// 프로그램은 여기서 끝난다.
+
+
+
+            //string personalGender = "여자";
+
+            //if (personalGender.Equals("남자"))
+            //{
+            //    // 조건문 A가 참(True)이면 여기로 옴
+            //    // 조건문 A가 여기서부터 읽기 시작한다.
+            //    Console.WriteLine("남자구나 ok");
+            //    // 조건문 A가 여기서 끝난다.
+            //}
+            //else if (personalGender == "여자")
+            //{
+            //    // 조건문 B가 참(True)이면 여기로 옴
+            //    // 조건문 B가 여기서부터 읽기 시작한다.
+            //    Console.WriteLine("여자 구나");
+            //    // 조건문 B가 여기서 끝난다.
+            //}
+            //else
+            //{
+            //    // 저 조건문이 거짓(False)이면 여기로 옴
+            //    // 조건문은 여기서부터 읽기 시작한다.
+            //    Console.WriteLine("남자도 여자도 아니구나");
+            //    // 조건문은 여기서 끝난다.
+            //}
+
+            //for (int index = 1; index <= 50; index++)
+            //{
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("정말로 콘솔이 정리가되었나?");
+
+            ////프로그램 사용자로부터 양의 정수를 하나 입력 받아서,
+            //// 그 수만큼 "Hello world"를 출력하는 프로그램 작성
+            //int someNumber = 100;  //어떤 숫자
+            //bool isPositiveInteger = (0 < someNumber);
+
+            ///*
+            // * 프로그램 사용자로부터 계속해서 정수를 입력 받는다.
+            // * 그리고 그 값을 계속해서 더해 나간다. 이러한 작업은
+            // * 프로그램 사용가 0을 입력할 때까지 계속되어야 하며,
+            // * 0이 입력되면 입력된 모든 정수의 합을 출력하고 프로그램 종료.
+            // */
+            //bool isNumberZero = (someNumber == 0);
+
+            ////프로그램 사용자로부터 입력 받은 숫자에 해당하는 구구단을 출력하되,
+            //// 역순으로 출력하는 프로그램을 작성
+            //int userInputNumber = 3;
+            //for (int index = 9; 1 <= index; index--)
+            //{
+            //    Console.WriteLine($"{userInputNumber} + {index} = {userInputNumber * index}");
+            //}  // loop : 9번을 도는 루프
+
+
+            ///*
+            // * 프로그램 사용자로부터 입력 받은 정수의 평균을 출력하는 프로그램을 작성하되,
+            // * 다음 두 가지의 조건을 만족할 것
+            // * - 먼저 몇 개의 정수를 입력할 것인지  프로그램 사용자에게 묻는다.
+            // * 그리고 그 수만큼 정수를 입력 받는다.
+            // * - 평균 값은 소수점 이하까지 계산해서 출력한다.
+            // */
+
+            ///* LAD 1 비밀 코드 맞추기
+            // * 컴퓨터가 숨기고 있는 비밀 코드를 추측하는 게임을 작성
+            // * 비밀 코드는 a~z 사이의 문자
+            // * 컴퓨터는 사용자의 추측을 읽고서 앞에 있는지, 뒤에 있는지를 알려준다.
+            // * (즉, 사용자에게 힌트를 준다.)
+            // */
+            //char scretCode = 'y';
+            //char userInputAlphabet = 'c';  // 유저가_입력한_알파벳
+            //bool isSmallAlphabet = ('a' <= userInputAlphabet && userInputAlphabet <= 'z');
+
+            //bool isAlphabetFore = (userInputAlphabet <= scretCode);
+            //bool isAlphabetBack = (scretCode <= userInputAlphabet);
+
+            //if (isSmallAlphabet) {/* Do nothing */ }
+            //else
+            //{
+            //    Console.WriteLine("{0} {1}", "[System] 당신의 입력은 처리할 수 없습니다.", "알파벳 소문자만을 입력해주세요.");
+            //}
+            //if (isAlphabetFore)
+            //{
+            //    Console.WriteLine("당신의 알파벳은 시크릿 코드보다 앞에 있습니다.");
+            //}
+            //else {/* Do nothing */ }
+
+            //if (isAlphabetBack)
+            //{
+            //    Console.WriteLine("당신의 알파벳은 시크릿 코드보다 뒤에 있습니다.");
+            //}
+            //else { /* Do nothing */}
+
+            //// 1~100 숫자 중에서 3의 배수이면서 4의 배수인 정수 합 구하기
+            //int sumNumber = 0;
+            //int someNumber = 100; // 어떤 수
+            //bool isMultipleofThree = (someNumber % 3 == 0);
+            //bool isMultipleofFour = (someNumber % 4 == 0);
+
+            //bool isSatisfyCondition = isMultipleofThree && isMultipleofFour;
+
+            //if (isSatisfyCondition)
+            //{
+            //    sumNumber += someNumber;
+            //}     // if: 3의 배수이며서 4의 배수인 값
+
+            //// 별을 100번 찍는 법 1번
+            //Console.WriteLine("{0} {1} {2} {3} {4}",
+            //    "**********", "**********", "**********", "**********");
+            //Console.WriteLine("{0} {1} {2} {3} {4}",
+            //    "**********", "**********", "**********", "**********");
+
+
+            //// 2번
+            //for (int index = 1; index <= 100; index++)
+            //{
+            //    Console.WriteLine("{0}", "*");
+            //}
+
+
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            ////3번
+            //for (int index = 1; index <= 10; index++)
+            //{
+            //    for (int index2 = 1; index2 <= 10; index2++)
+            //    {
+            //        Console.WriteLine("{0}", "*");
+            //    }  // loop: 이건 밖의 루프가 1번 도는 동안 10번을 도는 루프
+            //    Console.WriteLine();
+            //}    // loop: 이건 10번 도는 루프
+
+
+
+
+
+            //// 3중 for 문 쓰기
+            //int humdredCount = 0;
+            //for (int index = 1; index <= 10; index++)
+            //{
+            //    for (int index2 = 1; index2 <= 10; index2++)
+            //    {
+            //        for (int index3 = 1; index3 <= 10; index3++)
+            //        {
+            //            humdredCount++;
+            //            if (100 < humdredCount) { break; }
+            //            else {/* Do nothing */ }
+
+            //            //여기가 별을 찍는 지점
+            //            Console.WriteLine(" {0}", "*");
+
+            //            //여기서 10번마다 한 줄을 띄어 줄거임
+            //            if (humdredCount % 10 == 0)
+            //            {
+            //                Console.WriteLine();
+            //            } //if: 별을 10번 찍을 때마다 한 줄 띄어주는 중
+
+            //        }  // loop: 이건 1번 루프가 10번 도는 동안 2번 루프가 10번 돌고
+            //        // 그 동안 다시 10번을 도는 루프
+            //    }    // loop: 이건 밖의 루프가 1번 도는 동안 10번을 도는 루프
+            //}       // loop : 이건 10번을 도는 루프
+            //// 프로그램은 여기서 끝난다.
+
+
+
+
+
+            /* 문제 (마름모 모양도 만들어 보기)
+             * 유저 입력 받아서 (1~20 줄 이내로 입력 받음) 유저 입력은 줄, 단의 개수 임.
+             * 등차 수열로 한 단이 내려갈 때마다 별 1개씩 추가로 증가하는 프로그램 작성
+             * ex) user input: 5
+             *     *
+             *     **
+             *     ***
+             *     ****
+             *     *****
+             */
+
+            //int userInput = 0;
+
+            //Console.WriteLine(" 줄,단을 입력해주세요 : ");
+            //int.TryParse(Console.ReadLine(), out userInput);
+
+            //Console.WriteLine();
+
+            //for (int index1 = 1; index1 <= userInput; index1++)
+            //{
+            //    for (int index2 = 1; index2 <= index1; index2++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //다이아몬드 (미완성)
+            int userInput = 0;
+
+            Console.WriteLine(" 줄,단을 입력해주세요 : ");
+            int.TryParse(Console.ReadLine(), out userInput);
+
+            Console.WriteLine();
+
+            for (int index1 = 1; index1 <= userInput; index1++)
+            {
+                for (int index2 = 1; index2 <= (index1 *2); index2++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int index1 = 1; index1 >= userInput; index1--)
+            {
+                for (int index2 = 1; index2 <= ((index1-1)*2); index2++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
